8a019a8b7f420a85db6bf35b64180fe79426d4ec
gracelzr <gracelizirui@gmail.com>
2020-10-27 14:44:48 -0700

    Initial Implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,9 +8,16 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
+
+const element = React.createElement(
+	'h1',
+	{className:'greeting'},
+	'Hello, world!'
+);
+ReactDOM.render(element, document.getElementById('text'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
